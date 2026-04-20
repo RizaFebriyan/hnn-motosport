3b1e95bf74b752b4a5330912cd280e7aa4bafde8
RizaFebriyan <riza6500@gmail.com>
2026-04-20 15:35:49 +0700

    Add Detail Route & Detail Function

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('/', [PublicController::class, 'index'])->name('home');
 
 // Halaman Stok Motor
 Route::get('/stok-motor', [PublicController::class, 'stok'])->name('stok');
+Route::get('/stok-motor/{slug}', [PublicController::class, 'show'])->name('stok.show');
 
 // Halaman Tentang Kami
 Route::get('/tentang-kami', [PublicController::class, 'tentang'])->name('tentang');
